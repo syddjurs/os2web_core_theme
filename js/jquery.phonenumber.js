88455da8bb779269551270f6f9452c150e40d0ee
Anders Bryrup <andersbryrup@gmail.com>
2013-04-17 15:40:39 +0200

    First commit.

---
+++ js/jquery.phonenumber.js
@@ -0,0 +1,6 @@
+jQuery(document).ready(function($) {
+$("#tlf").text(function(i, text) {
+        text = text.replace(/(\d\d)(\d\d)(\d\d)(\d\d)/, "$1 $2 $3 $4");
+        return text;
+    });
+});
---
+++ os2web_core_theme.make
@@ -0,0 +1,8 @@
+api = 2
+core = 7.x
+
+projects[omega][type] = "theme"
+projects[omega][version] = "3.1"
+
+projects[skinr][type] = "module"
+projects[skinr][version] = "2.0-beta1"
